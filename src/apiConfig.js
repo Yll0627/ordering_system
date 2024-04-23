6e9a601f50b2d4f446881e3542e66956c7805b0e
Yueling Fan <yueling@kth.se>
2024-04-23 16:27:32 +0200

    /

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://brfenergi.se/iprog/group/16/";   
-const API_KEY = "3d2a031b4cmsh5cd4e7b939ada54p19f679jsn9a775627d767";
+const API_KEY = " ";
 
 export{BASE_URL, API_KEY}
 
